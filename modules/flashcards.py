338510cb8bef0a3af055687812839dc7a43fee58
Sinchana-R-1645 <sinchanar1645@gmail.com>
2026-03-28 16:07:57 +0530

    flashcards.py

--- modules/flashcards.py
+++ modules/flashcards.py
@@ -1,13 +1,21 @@
+import re
+
 def generate_flashcards(text):
 
-    sentences = [s.strip() for s in text.split('.') if len(s.strip()) > 15]
+    sentences = [s.strip() for s in re.split(r'[.!?]', text) if len(s.strip()) > 25]
 
     flashcards = []
 
     for s in sentences[:5]:
-        words = s.split()
-        front = " ".join(words[:4]) + "..."
-        back = s
+
+        if " is " in s:
+            parts = s.split(" is ", 1)
+            front = parts[0].strip()
+            back = parts[1].strip()
+        else:
+            words = s.split()
+            front = " ".join(words[:6]) + "..."
+            back = s
 
         flashcards.append({
             "front": front,
