87124705fb9341bfd63c0529101d2c55501c2ad6
spoorthiskulkarni31-oss <spoorthiskulkarni31@gmail.com>
2026-04-22 19:27:28 +0530

    utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -2,23 +2,25 @@ from groq import Groq
 import os
 import pdfplumber
 import docx
+from gtts import gTTS
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 MODEL = "llama-3.1-8b-instant"
 
 def llm(prompt):
-    return client.chat.completions.create(
+    response = client.chat.completions.create(
         model=MODEL,
-        messages=[{"role":"user","content":prompt}]
-    ).choices[0].message.content
-
+        messages=[{"role":"user","content":prompt}],
+        temperature=0.3
+    )
+    return response.choices[0].message.content
 
 def read_file(file):
     if file.name.endswith(".pdf"):
         text = ""
         with pdfplumber.open(file) as pdf:
-            for p in pdf.pages:
-                text += p.extract_text() or ""
+            for page in pdf.pages:
+                text += page.extract_text() or ""
         return text
 
     elif file.name.endswith(".docx"):
@@ -27,3 +29,8 @@ def read_file(file):
 
     else:
         return file.read().decode("utf-8")
+
+def generate_audio(text):
+    tts = gTTS(text=text[:3000], lang='en')
+    tts.save("summary.mp3")
+    return "summary.mp3"
