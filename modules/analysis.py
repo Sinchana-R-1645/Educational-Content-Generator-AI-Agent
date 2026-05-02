20ec5b20d73fec4bec2098773718fd7e63bd8096
Sinchana-R-1645 <sinchanar1645@gmail.com>
2026-05-02 21:44:05 +0530

    analysis.py

--- modules/analysis.py
+++ modules/analysis.py
@@ -1,1 +1,19 @@
+elif menu == "Analytics":
+    show_analysis()
 
+    data = get()
+
+    if data:
+        df = pd.DataFrame(data, columns=["Subject","Score","Total"])
+        df["Percentage"] = (df["Score"] / df["Total"]) * 100
+
+        st.dataframe(df)
+
+        plt.figure()
+        plt.plot(df["Percentage"], marker="o")
+        plt.title("Performance")
+        plt.ylabel("Percentage")
+        st.pyplot(plt)
+
+    else:
+        st.info("No data yet")
