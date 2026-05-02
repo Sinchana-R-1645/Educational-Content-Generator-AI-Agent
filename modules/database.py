8c678658e4a3ffb272e1436266ec7b504c762245
Sinchana-R-1645 <sinchanar1645@gmail.com>
2026-05-02 21:34:57 +0530

    database.py

--- modules/database.py
+++ modules/database.py
@@ -1,2 +1,23 @@
+%%writefile database.py
+import sqlite3
 
+conn = sqlite3.connect("study.db", check_same_thread=False)
+c = conn.cursor()
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS scores(
+subject TEXT,
+score INT,
+total INT
+)
+""")
+conn.commit()
+
+def save(subject, score, total):
+    c.execute("INSERT INTO scores VALUES(?,?,?)",(subject,score,total))
+    conn.commit()
+
+def get():
+    c.execute("SELECT subject,score,total FROM scores")
+    return c.fetchall()
 
